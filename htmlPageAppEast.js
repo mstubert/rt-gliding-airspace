609b4ca458a56f8744e0ff93a931ef802648cc84
mstubert <mstubert39@gmail.com>
2023-11-01 14:00:04 +0100

    Change presentTable fs to 17

--- htmlPageAppEast.js
+++ htmlPageAppEast.js
@@ -118,7 +118,7 @@ var htmlText1 = `
           .weatherTable {
             background: linear-gradient(to right, #d6d3d1, #f9fafc);
             color: #1c1917;
-            font-size: 15px;
+            font-size: 17px;
             border-radius: 15px;
             margin: 5px;
             padding: 20px;
